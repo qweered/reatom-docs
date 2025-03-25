0798bee352c16fcadbeeaa643ae75450fb416776
qweered <grubian2@gmail.com>
2025-03-25 13:12:14 +0200

    switch back to dynamic

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,7 +1,10 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
 
+// For static search
 // it should be cached forever
-export const revalidate = false;
+// export const revalidate = false;
  
-export const { staticGET: GET } = createFromSource(source);
+// export const { staticGET: GET } = createFromSource(source);
+
+export const { GET } = createFromSource(source);
--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,11 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider
-          search={{
-            options: {
-              type: 'static',
-            },
-          }}
+          // search={{
+          //   options: {
+          //     type: 'static',
+          //   },
+          // }}
         >
           {children}
         </RootProvider>
